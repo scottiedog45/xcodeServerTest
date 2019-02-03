c2b469ee6952d2595ee3b9a3a0d594a27ef36655
Scott O'Toole <scottiedog45@gmail.com>
2019-02-03 16:31:55 +0100

    Initial Commit

---
+++ funWithXcodeServer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  funWithXcodeServer
+//
+//  Created by Scott OToole on 2/3/19.
+//  Copyright © 2019 Scott OToole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
